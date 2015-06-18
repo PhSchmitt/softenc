5d1b180d0b00217e081091381000768cd12d6e48
Philipp <k_schmitt12@cs.uni-kl.de>
2015-06-18 13:54:16 +0200

    Splitting removed - everything is working fine

--- app/src/main/java/de/unikl/cs/disco/softenc/SoftencActivity.java
+++ app/src/main/java/de/unikl/cs/disco/softenc/SoftencActivity.java
@@ -23,9 +23,10 @@ public class SoftencActivity extends ActionBarActivity {
         System.loadLibrary("softenc");
     }
 
+    //constants
     final String hostname = "mptcpsrv1.philippschmitt.de";
     final Integer port = 8080;
-    final Integer maxPayloadLength = 372;
+    final char maskToClearDataId = 0x3fff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +37,7 @@ public class SoftencActivity extends ActionBarActivity {
 
         buttonData.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-//                 Perform action on click
                 String rawDataString = createRandomString();
-                Log.w("Send len: ", "" + rawDataString.length());
                 DataSet data = new DataSet(rawDataString.length());
                 data.fulldecryptedstream = rawDataString.toCharArray();
                 encryptStream(data);
@@ -258,132 +257,38 @@ public class SoftencActivity extends ActionBarActivity {
 
     private void sendData(String asend, String bsend, String csend, String dsend)
     {
-        boolean splitPackets = false;
-        final int stringLength = Math.max(asend.length(), Math.max(bsend.length(), Math.max(csend.length(), dsend.length())));
-        final int streamLength = (int) Math.ceil(stringLength / (double) maxPayloadLength);
-        String[] aStreams = new String[streamLength];
-        String[] bStreams = new String[streamLength];
-        String[] cStreams = new String[streamLength];
-        String[] dStreams = new String[streamLength];
         /**
          * Bits of dataIdLastStreamIndicatorAndPktCntr:
          * Bit15=most left bit
          * 15,14: dataId: 00=a' 01=b' 10=c' 11=d'
          * 13: must be 1 since leading zeros (especially chars that only contain 0s) get lost in conversion/transmission
-         * 12: isLastStream: indicates whether this was the last stream of that dataId (1) of if there are following ones (0)
-         * 11-0: PacketCounter => only 8191 packets are supported.
+         * 12: isLastStream: server listens until packets with bit 12 set for all IDs received
+         * 11-0: packet counter for future use
          *
          * Init: Bit 13 set, all others 0
          */
         char dataIdLastStreamIndicatorAndPktCntr = 0x2000;
-        final char maskToClearDataId = 0x3fff;
 
-        if (stringLength > maxPayloadLength) {
-            //TODO: works without splitting => fix splitting (or maybe recombination?)
-            //splitPackets = true;
-            int j = 0;
-            for (int i = 0; j < stringLength; i++)
-            {
-                aStreams[i] = asend.substring(j, Math.min(j + maxPayloadLength, asend.length()));
-                bStreams[i] = asend.substring(j, Math.min(j + maxPayloadLength, bsend.length()));
-                cStreams[i] = asend.substring(j, Math.min(j + maxPayloadLength, csend.length()));
-                dStreams[i] = asend.substring(j, Math.min(j + maxPayloadLength, dsend.length()));
-                j += maxPayloadLength;
-            }
-        }
         try {
-            if (!splitPackets) {
                 //set isLastStream to 1
                 dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0x1000), or);
-                sendAprimePkt(asend, dataIdLastStreamIndicatorAndPktCntr, maskToClearDataId);
+            sendAprimePkt(asend, dataIdLastStreamIndicatorAndPktCntr);
 
                 // TODO we let the server the time to open a new socket - should be deleted as soon as the Server listens continuously
                 Thread.sleep(100);
-                sendBprimePkt(bsend, dataIdLastStreamIndicatorAndPktCntr, maskToClearDataId);
+            sendBprimePkt(bsend, dataIdLastStreamIndicatorAndPktCntr);
 
                 Thread.sleep(100);
-                sendCprimePkt(csend, dataIdLastStreamIndicatorAndPktCntr, maskToClearDataId);
+            sendCprimePkt(csend, dataIdLastStreamIndicatorAndPktCntr);
 
                 Thread.sleep(100);
-                sendDprimePkt(dsend, dataIdLastStreamIndicatorAndPktCntr, maskToClearDataId);
-            } else //splitPackages
-            {
-                for (int i = 0; i < aStreams.length; i++) {
-                    if (aStreams.length - 1 == i) //is last stream => set last stream bit
-                    {
-                        dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0x1000), or);
-                    } else //is not last stream => clear last stream bit
-                    {
-                        dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0xEFFF), and);
-                    }
-                    int pktCntr = i;
-                    //ensure that only at most 12 bit are set
-                    pktCntr &= 0xFFF;
-                    dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0xF000), and);
-                    dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) pktCntr, or);
-                    // TODO we let the server the time to open a new socket - should be deleted as soon as the Server listens continuously
-                    Thread.sleep(100);
-                    sendAprimePkt(aStreams[i], dataIdLastStreamIndicatorAndPktCntr, maskToClearDataId);
-
-                }
-                for (int i = 0; i < bStreams.length; i++) {
-                    if (bStreams.length - 1 == i) //is last stream => set last stream bit
-                    {
-                        dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0x1000), or);
-                    } else //is not last stream => clear last stream bit
-                    {
-                        dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0xEFFF), and);
-                    }
-                    int pktCntr = i;
-                    //ensure that only at most 12 bit are set
-                    pktCntr &= 0xFFF;
-                    dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0xF000), and);
-                    dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) pktCntr, or);
-                    // TODO we let the server the time to open a new socket - should be deleted as soon as the Server listens continuously
-                    Thread.sleep(100);
-                    sendBprimePkt(bStreams[i], dataIdLastStreamIndicatorAndPktCntr, maskToClearDataId);
-                }
-                for (int i = 0; i < cStreams.length; i++) {
-                    if (cStreams.length - 1 == i) //is last stream => set last stream bit
-                    {
-                        dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0x1000), or);
-                    } else //is not last stream => clear last stream bit
-                    {
-                        dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0xEFFF), and);
-                    }
-                    int pktCntr = i;
-                    //ensure that only at most 12 bit are set
-                    pktCntr &= 0xFFF;
-                    dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0xF000), and);
-                    dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) pktCntr, or);
-                    // TODO we let the server the time to open a new socket - should be deleted as soon as the Server listens continuously
-                    Thread.sleep(100);
-                    sendCprimePkt(cStreams[i], dataIdLastStreamIndicatorAndPktCntr, maskToClearDataId);
-                }
-                for (int i = 0; i < dStreams.length; i++) {
-                    if (dStreams.length - 1 == i) //is last stream => set last stream bit
-                    {
-                        dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0x1000), or);
-                    } else //is not last stream => clear last stream bit
-                    {
-                        dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0xEFFF), and);
-                    }
-                    int pktCntr = i;
-                    //ensure that only at most 12 bit are set
-                    pktCntr &= 0xFFF;
-                    dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) (0xF000), and);
-                    dataIdLastStreamIndicatorAndPktCntr = maskChar(dataIdLastStreamIndicatorAndPktCntr, (char) pktCntr, or);
-                    // TODO we let the server the time to open a new socket - should be deleted as soon as the Server listens continuously
-                    Thread.sleep(100);
-                    sendDprimePkt(dStreams[i], dataIdLastStreamIndicatorAndPktCntr, maskToClearDataId);
-                }
-            }
-        } catch (Exception e) {
+            sendDprimePkt(dsend, dataIdLastStreamIndicatorAndPktCntr);
+            } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void sendDprimePkt(String dsend, char dataIdLastStreamIndicatorAndPktCntr, char maskToClearDataId) {
+    private void sendDprimePkt(String dsend, char dataIdLastStreamIndicatorAndPktCntr) {
         //send dprimes
         Log.d("Softenc", "Trying to send dprimes");
         //set dataId to 11 (=d)
@@ -398,7 +303,7 @@ public class SoftencActivity extends ActionBarActivity {
         }
     }
 
-    private void sendCprimePkt(String csend, char dataIdLastStreamIndicatorAndPktCntr, char maskToClearDataId) {
+    private void sendCprimePkt(String csend, char dataIdLastStreamIndicatorAndPktCntr) {
         //send cprimes
         Log.d("Softenc", "Trying to send cprimes");
         //set dataId to 10 (=c)
@@ -413,7 +318,7 @@ public class SoftencActivity extends ActionBarActivity {
         }
     }
 
-    private void sendBprimePkt(String bsend, char dataIdLastStreamIndicatorAndPktCntr, char maskToClearDataId) {
+    private void sendBprimePkt(String bsend, char dataIdLastStreamIndicatorAndPktCntr) {
         //send bprimes
         Log.d("Softenc", "Trying to send bprimes");
         //set dataId to 01 (=b)
@@ -428,7 +333,7 @@ public class SoftencActivity extends ActionBarActivity {
         }
     }
 
-    private void sendAprimePkt(String asend, char dataIdLastStreamIndicatorAndPktCntr, char maskToClearDataId) {
+    private void sendAprimePkt(String asend, char dataIdLastStreamIndicatorAndPktCntr) {
         //send aprimes
         Log.d("Softenc", "Trying to send aprimes");
         //set dataId to 00 (=a)
